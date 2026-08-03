ea3802948ddca6664c8c511ec78f250ed990005e
Priyal Prasad <prasadpriyal006@gmail.com>
2026-08-03 23:28:55 +0530

    Time: 38 ms (5.24%) | Memory: 110.7 MB (5.59%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // creating hashmap to store the frequencies of the elements
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int freq: map.values()){
+            if(freq > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
