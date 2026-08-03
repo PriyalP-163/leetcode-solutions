34b32b7e3b30a09c35b50b2e167beb3a8a7c2a2d
Priyal Prasad <prasadpriyal006@gmail.com>
2026-08-03 23:30:41 +0530

    Time: 17 ms (69.69%) | Memory: 108.1 MB (36.71%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,15 +1,27 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // creating hashmap to store the frequencies of the elements
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
-        for(int freq: map.values()){
-            if(freq > 1){
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num,0)+1);
+        // }
+        // for(int freq: map.values()){
+        //     if(freq > 1){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // using hashset
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return true;
             }
+            set.add(num);
         }
+
         return false;
     }
 }
