b9addb74ceaa1a5714f1744dfc86cd8f06154f90
Priyal Prasad <prasadpriyal006@gmail.com>
2026-08-06 01:17:48 +0530

    Time: 17 ms (17.94%) | Memory: 93.3 MB (6.07%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -6,7 +6,7 @@ class Solution {
         for(int num : nums){
             set.add(num);
         }
-        for(int i=0; i<nums.length+1; i++){
+        for(int i=0; i<nums.length+1; i++){ // length of nums+1 because of handling the case when all the smallest elements present so the counter will be increased but not print due to end of loop
             if(!set.contains(count)) // if hashset doesnot conatins the currecct count value (starts from 1) then returnn that smallest positive number
             { 
                 return count;
