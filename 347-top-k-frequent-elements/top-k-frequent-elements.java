9a5471b5bacc00949257ffb56f4ca547aeba56fb
Priyal Prasad <prasadpriyal006@gmail.com>
2026-08-14 23:27:18 +0530

    Time: 14 ms (70.45%) | Memory: 47.8 MB (33.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,16 +1,29 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> shreyal = new HashMap<>();
-        for(int num : nums){
-            shreyal.put(num, shreyal.getOrDefault(num,0)+1);
+        // HashMap<Integer, Integer> shreyal = new HashMap<>();
+        // for(int num : nums){
+        //     shreyal.put(num, shreyal.getOrDefault(num,0)+1);
+        // }
+        // List<Map.Entry<Integer, Integer>> list = new ArrayList<>(shreyal.entrySet());
+        //     list.sort(Map.Entry.comparingByValue());
+
+        // int ans[] = new int[k];
+        // for(int i=0; i<k; i++){
+        //     ans[i] = list.get(list.size() -1-i).getKey();
+        // }
+        // return ans;    
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
         }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(shreyal.entrySet());
-            list.sort(Map.Entry.comparingByValue());
+        PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+        q.addAll(map.entrySet());
 
         int ans[] = new int[k];
         for(int i=0; i<k; i++){
-            ans[i] = list.get(list.size() -1-i).getKey();
+            ans[i] = q.poll().getKey();
         }
-        return ans;    
+        return ans;
     }
 }
