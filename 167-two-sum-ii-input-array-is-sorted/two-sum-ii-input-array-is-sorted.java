6a872328b900900b6d0217887aa9e04b9a7e5e08
Priyal Prasad <prasadpriyal006@gmail.com>
2026-08-15 16:37:20 +0530

    Time: 4 ms (14.75%) | Memory: 48.4 MB (42.35%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,19 +1,42 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int left =0;
-        int right = numbers.length-1;
-        while(left < right){
-            int sum = numbers[left]+numbers[right];
-            if (sum == target){
-                return new int []{left+1, right+1};
+        // 1. let's try this problem only with two pointers approach
+        // int left =0;
+        // int right = numbers.length-1;
+        // while(left < right){
+        //     int sum = numbers[left]+numbers[right];
+        //     if (sum == target){
+        //         return new int []{left+1, right+1};
+        //     }
+        //     else if (target > sum){
+        //         left++;
+        //     }
+        //     else{
+        //         right--;
+        //     }
+        // }
+        // return new int[]{};
+
+
+        // 2. now let's try this problem using the combination of binary search and Two pointers
+        for(int i=0; i< numbers.length-1; i++){
+            int required = target - numbers[i];
+            int left = i+1;
+            int right = numbers.length-1;
+            while(left <= right){
+                int mid = left + (right-left)/2;
+                if(required == numbers[mid]){
+                    return new int[] {i+1, mid+1};
+                }
+                else if (required > numbers[mid]){
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1;;
+                }
             }
-            else if (target > sum){
-                left++;
-            }
-            else{
-                right--;
-            }
+           
         }
-        return new int[]{};
+         return new int[]{};
     }
 }
